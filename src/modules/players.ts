ee569d568cb81d583246d0a4cdd2baa3c12951b9
Parker Lee <plee@parkers-mbp.attlocal.net>
2022-08-15 21:52:00 -0500

    Do not require leagueId to getPlayers

--- src/modules/players.ts
+++ src/modules/players.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from '../config/variables';
 import fetchData from '../config/fetch';
 
-export async function getPlayers(leagueId: string) {
+export async function getPlayers() {
   return await fetchData(`${baseUrl}/players/nfl`);
 }

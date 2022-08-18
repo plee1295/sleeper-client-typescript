278d62017f5b4c8401cd9e1e8e59c1cd00744816
Parker Lee <plee@parkers-mbp.attlocal.net>
2022-08-17 20:22:35 -0500

    Clean up responses

--- src/config/error.ts
+++ src/config/error.ts
@@ -9,36 +9,32 @@ export default function handleError(res: any) {
       };
     case 404:
       return {
-        status: 400,
+        status: 404,
         error: 'Not Found',
         message:
           'The server has not found anything matching the Request-URI. No indication is given of whether the condition is temporary or permanent.',
       };
     case 429:
       return {
-        status: 400,
+        status: 429,
         error: 'Too Many Requests',
         message:
           'The user has sent too many requests in a given amount of time. Intended for use with rate limiting schemes.',
       };
     case 500:
       return {
-        status: 400,
+        status: 500,
         error: 'Internal Server Error',
         message: 'The server encountered an unexpected condition which prevented it from fulfilling the request.',
       };
     case 503:
       return {
-        status: 400,
+        status: 503,
         error: 'Service Unavailable',
         message:
           'The server is currently unable to handle the request due to a temporary overloading or maintenance of the server. The implication is that this is a temporary condition which will be alleviated after some delay. If known, the length of the delay MAY be indicated in a Retry-After header. If no Retry-After is given, the client SHOULD handle the response as it would for a 500 response.',
       };
     default:
-      return {
-        status: 200,
-        message: 'Success',
-        data: res,
-      };
+      return res;
   }
 }
